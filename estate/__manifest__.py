f06ed4796d9f4a4dc6bd8c96fcfcbe2f3d4ee143
Emilio <emiliog@vauxoo.com>
2024-09-27 17:10:19 -0600

    [ADD]  Module estate: Develppment of the tutorial server framework 101
    This commit addresses various code adjustments based on the development of the exercises included on the tutorial server framework 101 in Odoo docs, which covers the basic topics to know Odoo.
    Here is a summary of what was done in each chapter:
    - Chapter 1: Architecture overview, how the application is organized
    - Chapter 2: Creation of **init**.py and **manifest**.py files of the new module
    - Chapter 3: Creation of estate model and adding some attributes
    - Chapter 4: Adding ir.model.access.csv file for access security
    - Chapter 5: First aproach to UI, creation of menus and actions for my model
    - Chapter 6: Review of the basic views, adding custom forms, list and search for filtering
    - Chapter 7: Relations between models, adding estate.property.type, estate.property.tag and estate.property.offer models and establishing its relations
    - Chapter 8: Aproach to compute fields, inverse functions and onchanges.
    - Chapter 9: Adding buttons and implementing some business logic behind them
    - Chapter 10: Review of SQL constraints and python constraints, adding some of them in my code
    - Chapter 11: More detail design: inline views, widgets, list ordering, attrbutes like invisible, readonly, hidden, and adding a stat button
    
    Additionally, it includes fixes for writing errors, pre-commit linting, and
    mandatory checks of pre-commit-vauxoo.
    
    Closes #1

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Estate",
+    "description": "Module based on tutorial server framework 101",
+    "author": "Emilio Gonzalez",
+    "depends": [
+        "base_setup",
+    ],
+    'data': [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_offer.xml",
+        "views/estate_type_views.xml",
+        "views/estate_menus.xml",
+    ],
+    "license": "LGPL-3"
+}
